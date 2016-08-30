80501b154cfde01482a4c79de4d2f3a85edd827b
Chen Jian <lsdlab@icloud.com>
2016-08-30 21:22:23 +0800

    add roman to integer solution

---
+++ roman_to_integer.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman_map = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        result = 0
+        for i in range(len(s)):
+            if i > 0 and roman_map[s[i]] > roman_map[s[i - 1]]:
+                result += roman_map[s[i]] - 2 * roman_map[s[i - 1]]
+            else:
+                result += roman_map[s[i]]
+        return result
